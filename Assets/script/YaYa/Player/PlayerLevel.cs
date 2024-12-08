3fdd9f02ef7ea9f4619a26ddbdf6bf522eca3c3c
ShengYe987 <stanley638738@gmail.com>
2024-12-08 16:05:04 +0800

    增加經驗及等級系統

---
+++ Assets/script/YaYa/Player/PlayerLevel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLevel : MonoBehaviour
+{
+    public main PlayerXp;
+    public Image MaxXp;
+    public Image CurrentXp;
+
+    public float XP;
+    private void Start()
+    {
+        MaxXp.fillAmount= PlayerXp.Upgrade;
+        
+    }
+    private void Update()
+    {XP = PlayerXp.Upgrade;
+        CurrentXp.fillAmount = PlayerXp.Xp/XP;
+    }
+}
--- Assets/script/move.cs
+++ Assets/script/move.cs
@@ -38,5 +38,19 @@ public class move : MonoBehaviour
         }
         direction = direction.normalized;
         transform.Translate(direction * speed * Time.deltaTime);
+    }/* private float HorizontalInput;
+    private float VerticalInput;
+    public float speed=1;
+    void Start()
+    {
+        
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+       transform.Translate(HorizontalInput*speed*Time.deltaTime,VerticalInput*speed*Time.deltaTime,0);
+    }*/
 }
